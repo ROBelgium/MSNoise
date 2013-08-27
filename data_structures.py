8ba51dfa009e8e60374ca19edc40a4eb7a7394a7
ThomasLecocq <thomas.lecocq@oma.be>
2013-08-27 22:16:29 +0200

    required files

---
+++ data_structures.py
@@ -0,0 +1,6 @@
+#Data Structure Definitions:
+data_structure = {}
+data_structure['SDS'] = "YEAR/NET/STA/CHAN.D/NET.STA.LOC.CHAN.TYPE.YEAR.DAY"
+data_structure['BUD'] = "NET/STA/STA.NET.LOC.CHAN.YEAR.DAY"
+data_structure['IDDS'] = "YEAR/NET/STA/CHAN.TYPE/DAY/NET.STA.LOC.CHAN.TYPE.YEAR.DAY.HOUR"
+data_structure['PDF'] = "YEAR/STA/CHAN.TYPE/NET.STA.LOC.CHAN.TYPE.YEAR.DAY"
