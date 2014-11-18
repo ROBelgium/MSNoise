98447565171d87a959689b72c5a158509756d385
ThomasLecocq <thomas.lecocq@oma.be>
2014-11-18 17:01:21 +0100

    default dtt_lag is static
    I don't expect station coordinates to be available!

--- msnoise/default.py
+++ msnoise/default.py
@@ -47,7 +47,7 @@ default['mov_stack'] = ["Number of days to stack for the Moving-window stacks ([
 default['export_format'] = ["Export stacks in which format(s) ? SAC/MSEED/[BOTH]","MSEED"]
 default['sac_format'] = ["Format for SAC stacks ? [doublets]/clarke","doublets"]
 
-default['dtt_lag'] = ["How is the lag window defined [dynamic]/static","dynamic"]
+default['dtt_lag'] = ["How is the lag window defined [dynamic]/static","static"]
 default['dtt_v'] = ["If dtt_lag=dynamic: what velocity to use to avoid ballistic waves [1.0]km/s","1.0"]
 default['dtt_minlag'] = ["If dtt_lag=static: min lag time","5.0"]
 default['dtt_width'] = ["Width of the time lag window [30]s","30.0"]
