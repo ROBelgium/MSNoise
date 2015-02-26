08e83b55e44e8e08eb6842848dc5e4c0f1d4dd25
ThomasLecocq <thomas.lecocq@oma.be>
2015-02-26 07:58:52 +0100

    test 101 :-)

--- msnoise/test/tests.py
+++ msnoise/test/tests.py
@@ -15,7 +15,7 @@ class MSNoiseTests(unittest.TestCase):
         except:
             traceback.print_exc()
             self.fail()
-    
+
     def test_002_ConnectToDB(self):
         from ..api import connect
         try:
@@ -23,13 +23,13 @@ class MSNoiseTests(unittest.TestCase):
             db.close()
         except:
             self.fail("Can't connect to MSNoise DB")
-    
+
     def test_003_set_and_config(self):
         from ..api import connect, get_config, update_config
         db = connect()
         totests = []
         path = os.path.abspath(os.path.dirname(__file__))
-        totests.append(['data_folder', os.path.join(path,'data')])
+        totests.append(['data_folder', os.path.join(path, 'data')])
         totests.append(['data_structure', 'PDF'])
         totests.append(['network', 'YA'])
         totests.append(['ZR', 'N'])
@@ -40,9 +40,9 @@ class MSNoiseTests(unittest.TestCase):
         totests.append(['RZ', 'N'])
         totests.append(['RR', 'N'])
         totests.append(['RT', 'N'])
-        
+
         for test in totests:
-            update_config(db,test[0], test[1])
+            update_config(db, test[0], test[1])
             d = get_config(db, test[0])
             self.failUnlessEqual(test[1], d)
 
@@ -54,19 +54,19 @@ class MSNoiseTests(unittest.TestCase):
         db = connect()
         filters = []
         filters.append(Filter(0.01, 0.12, 1.0, 0.98, 0, 10, 5, 1))
-        filters.append(Filter(0.1, 0.12, 1.0, 0.98, 0, 10, 5, 1))
-        
+        filters.append(Filter(0.1, 0.12, 1.0, 0.98,  0, 10, 5, 1))
+
         for f in filters:
             update_filter(db, f.ref, f.low, f.mwcs_low, f.high, f.mwcs_high,
-                  f.rms_threshold, f.mwcs_wlen, f.mwcs_step, f.used)
-        
+                          f.rms_threshold, f.mwcs_wlen, f.mwcs_step, f.used)
+
         dbfilters = get_filters(db)
         for i, filter in enumerate(dbfilters):
             for param in ['low', 'mwcs_low', 'high', 'mwcs_high',
                           'rms_threshold', 'mwcs_wlen', 'mwcs_step', 'used']:
-                self.failUnlessEqual(eval("filter.%s"%param),
-                                     eval("filters[i].%s"%param))
-    
+                self.failUnlessEqual(eval("filter.%s" % param),
+                                     eval("filters[i].%s" % param))
+
     def test_005_populate_station_table(self):
         from ..s002populate_station_table import main
         try:
@@ -74,68 +74,73 @@ class MSNoiseTests(unittest.TestCase):
             self.failUnlessEqual(ret, True)
         except:
             self.fail()
-    
+
     def test_006_get_stations(self):
         from ..api import connect, get_stations
         db = connect()
         stations = get_stations(db).all()
         self.failUnlessEqual(len(stations), 3)
         db.close()
-        
+
     def test_007_update_stations(self):
         from ..api import connect, get_stations, update_station
         import pandas as pd
         db = connect()
         path = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
-        stations = pd.read_csv(os.path.join(path, 'test','extra','stations.csv'),header=None, index_col = 0, names =['X','Y','altitude'])
+        stations = pd.read_csv(os.path.join(path, 'test', 'extra',
+                                            'stations.csv'),
+                               header=None, index_col=0,
+                               names=['X', 'Y', 'altitude'])
         for station in get_stations(db):
             fullname = "%s.%s" % (station.net, station.sta,)
             try:
                 s = stations.loc[fullname]
-                update_station(db, station.net, station.sta, s['X'], s['Y'], s['altitude'])
+                update_station(db, station.net, station.sta, s['X'],
+                               s['Y'], s['altitude'])
             except:
                 traceback.print_exc()
                 self.fail()
                 continue
             del s
-        
+
     def test_008_scan_archive(self):
         from ..s01scan_archive import main
         try:
-            main(init=True,threads=1)
+            main(init=True, threads=1)
         except:
             traceback.print_exc()
             self.fail()
-    
+
     def test_009_control_data_availability(self):
-        from ..api import connect, get_new_files, get_data_availability, count_data_availability_flags, get_stations
-        
+        from ..api import connect, get_new_files, get_data_availability,\
+            count_data_availability_flags, get_stations
+
         db = connect()
         files = get_new_files(db)
         self.failUnlessEqual(len(files), 3)
-        
+
         flags = count_data_availability_flags(db)
         self.failUnlessEqual(len(flags), 1)
-        
+
         for station in get_stations(db):
-            da = get_data_availability(db, net=station.net, sta=station.sta, comp='HHZ')
+            da = get_data_availability(db, net=station.net, sta=station.sta,
+                                       comp='HHZ')
             self.failUnlessEqual(len(da), 1)
-        
-    
+
     def test_010_new_jobs(self):
         from ..s02new_jobs import main
-        
+
         try:
             main()
         except:
             traceback.print_exc()
             self.fail()
-    
+
     def test_011_control_jobs(self):
         from ..api import connect, is_next_job, get_next_job
         from ..msnoise_table_def import Job
         db = connect()
-        
+
         self.failUnlessEqual(is_next_job(db), True)
         jobs = get_next_job(db)
         self.failUnlessEqual(isinstance(jobs[0], Job), True)
@@ -164,18 +169,18 @@ class MSNoiseTests(unittest.TestCase):
     def test_015_check_cc_files(self, format="MSEED"):
         from ..api import connect, get_filters, get_station_pairs, \
             get_components_to_compute
-        db =connect()
+        db = connect()
         for filter in get_filters(db):
             for components in get_components_to_compute(db):
                 for (sta1, sta2) in get_station_pairs(db):
                     pair = "%s_%s_%s_%s" % (sta1.net, sta1.sta,
                                             sta2.net, sta2.sta)
                     tmp = os.path.join("STACKS",
-                                        "%02i"%filter.ref,
-                                        "001_DAYS",
-                                        components,
-                                        pair,
-                                        "2010-09-01.%s"%format)
+                                       "%02i" % filter.ref,
+                                       "001_DAYS",
+                                       components,
+                                       pair,
+                                       "2010-09-01.%s" % format)
                     print "checking", tmp
                     if not os.path.isfile(tmp):
                         self.fail()
@@ -183,7 +188,7 @@ class MSNoiseTests(unittest.TestCase):
     def test_016_update_config(self):
         from ..api import connect, update_config
         db = connect()
-        update_config(db,"export_format", "SAC")
+        update_config(db, "export_format", "SAC")
 
     def test_017_reset_cc_jobs(self):
         from ..api import connect, reset_jobs
@@ -196,21 +201,45 @@ class MSNoiseTests(unittest.TestCase):
     def test_019_check_SACS(self):
         self.test_015_check_cc_files(format='SAC')
 
-
     def test_020_update_config(self):
         from ..api import connect, update_config
         import shutil
         shutil.rmtree("STACKS")
         db = connect()
-        update_config(db,"export_format", "BOTH")
+        update_config(db, "export_format", "BOTH")
 
     def test_021_reprocess_BOTH(self):
         self.test_017_reset_cc_jobs()
         self.test_013_s03compute_cc()
         self.test_015_check_cc_files(format='MSEED')
         self.test_015_check_cc_files(format='SAC')
-        
 
+    def test_022_check_content(self):
+        from obspy.core import read
+        from numpy.testing import assert_allclose
+        from ..api import connect, get_filters, get_station_pairs, \
+            get_components_to_compute
+        db = connect()
+        for filter in get_filters(db):
+            for components in get_components_to_compute(db):
+                for (sta1, sta2) in get_station_pairs(db):
+                    pair = "%s_%s_%s_%s" % (sta1.net, sta1.sta,
+                                            sta2.net, sta2.sta)
+                    tmp1 = os.path.join("STACKS",
+                                        "%02i" % filter.ref,
+                                        "001_DAYS",
+                                        components,
+                                        pair,
+                                        "2010-09-01.MSEED")
+                    tmp2 = os.path.join("STACKS",
+                                        "%02i" % filter.ref,
+                                        "001_DAYS",
+                                        components,
+                                        pair,
+                                        "2010-09-01.SAC")
+                    tmp1 = read(tmp1)
+                    tmp2 = read(tmp2)
+                    assert_allclose(tmp1[0].data, tmp2[0].data)
 
 
 def main():
