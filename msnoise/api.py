66919e6135606b687fa52d110880daa3cb0186fc
ThomasLecocq <thomas.lecocq@oma.be>
2016-10-25 17:19:18 +0200

    inf debug

--- msnoise/api.py
+++ msnoise/api.py
@@ -962,7 +962,7 @@ def stack(session, data):
     pws_timegate = float(get_config(session, 'pws_timegate'))
     pws_power = float(get_config(session, 'pws_power'))
     goal_sampling_rate = float(get_config(session, "cc_sampling_rate"))
-    print(data)
+    print("All finite?", np.all(np.isfinite(data)))
     data = data[~np.isnan(data).any(axis=1)]
     if stack_method == "linear":
         corr = data.mean(axis=0)
