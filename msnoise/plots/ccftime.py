74220b79f760df59172912a221ad93e4cf6ea079
Damian Kula <dkula@unistra.fr>
2017-10-23 17:38:12 +0200

    Refactors ccftime plot to use objects

--- msnoise/plots/ccftime.py
+++ msnoise/plots/ccftime.py
@@ -34,7 +34,9 @@ def main(sta1, sta2, filterid, components, mov_stack=1, ampli=5, seismic=False,
     cc_sampling_rate = float(get_config(db, 'cc_sampling_rate'))
     start, end, datelist = build_movstack_datelist(db)
     base = mdates.date2num(start) 
-    plt.figure(figsize=(12, 9))
+
+    fig = plt.figure(figsize=(12, 9))
+
     sta1 = sta1.replace('.', '_')
     sta2 = sta2.replace('.', '_')
     t = np.arange(samples)/cc_sampling_rate - maxlag
@@ -51,7 +53,7 @@ def main(sta1, sta2, filterid, components, mov_stack=1, ampli=5, seismic=False,
                                             mov_stack))
         nstack, stack_total = get_results(db, sta1, sta2, filterid, components,
                                           datelist, mov_stack, format="matrix")
-        ax = plt.subplot(111)
+        ax = fig.add_subplot(111)
         for i, line in enumerate(stack_total):
             if np.all(np.isnan(line)):
                 continue
@@ -61,11 +63,11 @@ def main(sta1, sta2, filterid, components, mov_stack=1, ampli=5, seismic=False,
             if envelope:
                 line = obspy_envelope(line)
             line /= line.max()
-            plt.plot(t, line * ampli + i + base, c='k')
+            ax.plot(t, line * ampli + i + base, c='k')
             if seismic:
                 y1 = np.ones(len(line)) * i
                 y2 = line*ampli + i + base
-                plt.fill_between(t, y1, y2, where=y2 >= y1, facecolor='k',
+                ax.fill_between(t, y1, y2, where=y2 >= y1, facecolor='k',
                                  interpolate=True)
 
         for filterdb in get_filters(db, all=True):
@@ -74,19 +76,19 @@ def main(sta1, sta2, filterid, components, mov_stack=1, ampli=5, seismic=False,
                 high = float(filterdb.high)
                 break
        
-        plt.xlabel("Lag Time (s)")
-        plt.axhline(0, lw=0.5, c='k')
-        plt.grid()
+        ax.set_xlabel("Lag Time (s)")
+        ax.axhline(0, lw=0.5, c='k')
+        ax.grid()
         title = '%s : %s, %s, Filter %d (%.2f - %.2f Hz), Stack %d' %\
                 (sta1.replace('_', '.'), sta2.replace('_', '.'), components,
                  filterid, low, high, mov_stack)
         if refilter:
             title += ", Re-filtered (%.2f - %.2f Hz)" % (freqmin, freqmax)
-        plt.title(title)
-        plt.scatter(0, [start, ], alpha=0)
-        plt.ylim(start-datetime.timedelta(days=ampli),
+        ax.set_title(title)
+        ax.scatter(0, [start, ], alpha=0)
+        ax.set_ylim(start-datetime.timedelta(days=ampli),
                  end+datetime.timedelta(days=ampli))
-        plt.xlim(-maxlag, maxlag)
+        ax.set_xlim(-maxlag, maxlag)
         ax.fmt_ydata = mdates.DateFormatter('%Y-%m-%d')
         cursor = Cursor(ax, useblit=True, color='red', linewidth=1.2)
 
@@ -99,6 +101,8 @@ def main(sta1, sta2, filterid, components, mov_stack=1, ampli=5, seismic=False,
                                                                   mov_stack))
             outfile = "ccftime " + outfile
             print("output to:", outfile)
-            plt.savefig(outfile)
+            fig.savefig(outfile)
         if show:
-            plt.show()
+            fig.show()
+        else:
+            plt.close(fig)
