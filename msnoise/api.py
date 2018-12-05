ROBelgium/MSNoise

commit 6cefc25aa9504494b3c8d84a9d57ece65ac87ebf

Fix use of tables prefix in database creation
msnoise_table_def.py was defining the table names and reading the prefix
from the db.ini file at compilation time, right after the interpreter
starts (as the code was at module level, in the class declaration).

As a consequence, on a new project (with no db.ini file), the prefix
chosen by the user was added to the db.ini file but ignored when
creating the tables, which were always created without any prefix. When
re-initialising the database (when a db.ini already exists), only the
prefix written in db.ini during the last run of 'msnoise db init' was
used, and again, the prefix entered by the user was ignored and simply
stored in db.ini.

As SQLAlchemy Declarative system sets up the classes mapped to
relational database tables as soon as they are defined, this definition
has to be postponed to run-time to allow the table names to use a prefix
entered by the user.

The changes introduced by this commit move the definition of the mapped
classes to a function to allow the use of a prefix entered before by the
user (when initialising a new database) or read from the db.ini file
(when using an existing database).

Modules using these mapped classes are updated to call the function
defining the mapped class instead of directly importing the classes.

Additionally, the s000installer.py file is reworked for better
readability and maintainability:

 - indices are created in their own function

 - user input for database and password choice somewhat improved

 - improved readability and maintainability of code handling default
   values in user choices

 - for security, no default password is provided (to avoid weak default
   installation)

msnoise/api.py
@@ -30,7 +30,17 @@ from obspy import read_inventory
 from obspy.io.xseed import Parser
 from obspy.geodetics import gps2dist_azimuth
 
-from .msnoise_table_def import Filter, Job, Station, Config, DataAvailability
+from .msnoise_table_def import declare_tables
+
+
+# Declare mapped tables
+sqlschema = declare_tables()
+Filter = sqlschema.Filter
+Job = sqlschema.Job
+Config = sqlschema.Config
+Station = sqlschema.Station
+DataAvailability = sqlschema.DataAvailability
+
 
 # TODO do we really need this here?
 plugin_tables = {}
@@ -280,7 +290,7 @@ def get_filters(session, all=False):
     :param all: Returns all filters from the database if True, or only filters
         where `used` = 1 if False (default)
 
-    :rtype: list of :class:`~msnoise.msnoise_table_def.Filter`
+    :rtype: list of :class:`~msnoise.msnoise_table_def.declare_tables.Filter`
     :returns: a list of Filter
     """
 
@@ -295,7 +305,7 @@ def update_filter(session, ref, low, mwcs_low, high, mwcs_high,
                   rms_threshold, mwcs_wlen, mwcs_step, used):
     """Updates or Insert a new Filter in the database.
 
-    .. seealso:: :class:`msnoise.msnoise_table_def.Filter`
+    .. seealso:: :class:`msnoise.msnoise_table_def.declare_tables.Filter`
 
     :type session: :class:`sqlalchemy.orm.session.Session`
     :param session: A :class:`~sqlalchemy.orm.session.Session` object, as
@@ -382,8 +392,8 @@ def get_stations(session, all=False, net=None):
     :type net: str
     :param net: if set, limits the stations returned to this network
 
-    :rtype: list of :class:`msnoise.msnoise_table_def.Station`
-    :returns: list of :class:`~msnoise.msnoise_table_def.Station`
+    :rtype: list of :class:`msnoise.msnoise_table_def.declare_tables.Station`
+    :returns: list of :class:`~msnoise.msnoise_table_def.declare_tables.Station`
     """
     q = session.query(Station)
     if all:
@@ -412,8 +422,8 @@ def get_station(session, net, sta):
     :type sta: str
     :param sta: the station code
 
-    :rtype: :class:`msnoise.msnoise_table_def.Station`
-    :returns: a :class:`~msnoise.msnoise_table_def.Station` Object
+    :rtype: :class:`msnoise.msnoise_table_def.declare_tables.Station`
+    :returns: a :class:`~msnoise.msnoise_table_def.declare_tables.Station` Object
 
     """
     station = session.query(Station).filter(Station.net == net).\
@@ -425,7 +435,7 @@ def update_station(session, net, sta, X, Y, altitude, coordinates='UTM',
                    instrument='N/A', used=1):
     """Updates or Insert a new Station in the database.
 
-    .. seealso :: :class:`msnoise.msnoise_table_def.Station`
+    .. seealso :: :class:`msnoise.msnoise_table_def.declare_tables.Station`
     
     :type session: :class:`sqlalchemy.orm.session.Session`
     :param session: A :class:`~sqlalchemy.orm.session.Session` object, as
@@ -480,7 +490,7 @@ def get_station_pairs(session, used=None, net=None):
     :param net: Network code to filter for the pairs.
 
     :rtype: iterable
-    :returns: An iterable of :class:`~msnoise.msnoise_table_def.Station` object
+    :returns: An iterable of :class:`~msnoise.msnoise_table_def.declare_tables.Station` object
         pairs
     """
     stations = get_stations(session, all=False, net=net)
@@ -495,9 +505,9 @@ def get_interstation_distance(station1, station2, coordinates="DEG"):
 
     .. warning:: Currently the stations coordinates system have to be the same!
 
-    :type station1: :class:`~msnoise.msnoise_table_def.Station`
+    :type station1: :class:`~msnoise.msnoise_table_def.declare_tables.Station`
     :param station1: A Station object
-    :type station2: :class:`~msnoise.msnoise_table_def.Station`
+    :type station2: :class:`~msnoise.msnoise_table_def.declare_tables.Station`
     :param station2: A Station object
     :type coordinates: str
     :param coordinates: The coordinates system. "DEG" is WGS84 latitude/
@@ -598,7 +608,7 @@ def get_new_files(session):
         obtained by :func:`connect`
 
     :rtype: list
-    :returns: list of :class:`~msnoise.msnoise_table_def.DataAvailability`
+    :returns: list of :class:`~msnoise.msnoise_table_def.declare_tables.DataAvailability`
     """
 
     files = session.query(DataAvailability).\
@@ -610,7 +620,7 @@ def get_new_files(session):
 def get_data_availability(session, net=None, sta=None, comp=None,
                           starttime=None, endtime=None):
     """
-    Returns the :class:`~msnoise.msnoise_table_def.DataAvailability` objects
+    Returns the :class:`~msnoise.msnoise_table_def.declare_tables.DataAvailability` objects
     for specific `net`, `sta`, `starttime` or `endtime`
 
     :type session: :class:`sqlalchemy.orm.session.Session`
@@ -626,7 +636,7 @@ def get_data_availability(session, net=None, sta=None, comp=None,
     :param endtime: End time of the search
 
     :rtype: list
-    :returns: list of :class:`~msnoise.msnoise_table_def.DataAvailability`
+    :returns: list of :class:`~msnoise.msnoise_table_def.declare_tables.DataAvailability`
     """
 
     if not starttime:
@@ -650,7 +660,7 @@ def get_data_availability(session, net=None, sta=None, comp=None,
 def mark_data_availability(session, net, sta, flag):
     """
     Updates the flag of all
-    :class:`~msnoise.msnoise_table_def.DataAvailability` objects matching
+    :class:`~msnoise.msnoise_table_def.declare_tables.DataAvailability` objects matching
     `net.sta` in the database
 
     :type session: :class:`sqlalchemy.orm.session.Session`
@@ -674,7 +684,7 @@ def mark_data_availability(session, net, sta, flag):
 
 def count_data_availability_flags(session):
     """
-    Count the number of :class:`~msnoise.msnoise_table_def.DataAvailability`,
+    Count the number of :class:`~msnoise.msnoise_table_def.declare_tables.DataAvailability`,
     grouped by `flag`
 
     :type session: :class:`sqlalchemy.orm.session.Session`
@@ -696,7 +706,7 @@ import time
 def update_job(session, day, pair, jobtype, flag, commit=True, returnjob=True,
                ref=None):
     """
-    Updates or Inserts a new :class:`~msnoise.msnoise_table_def.Job` in the
+    Updates or Inserts a new :class:`~msnoise.msnoise_table_def.declare_tables.Job` in the
     database.
 
     :type day: str
@@ -714,7 +724,7 @@ def update_job(session, day, pair, jobtype, flag, commit=True, returnjob=True,
         (False)
 
 
-    :rtype: :class:`~msnoise.msnoise_table_def.Job` or None
+    :rtype: :class:`~msnoise.msnoise_table_def.declare_tables.Job` or None
     :returns: If returnjob is True, returns the modified/inserted Job.
     """
     if ref:
@@ -740,11 +750,11 @@ def update_job(session, day, pair, jobtype, flag, commit=True, returnjob=True,
 def massive_insert_job(jobs):
     """
     Routine to use a low level function to insert much faster a list of
-    :class:`~msnoise.msnoise_table_def.Job`. This method uses the Engine
+    :class:`~msnoise.msnoise_table_def.declare_tables.Job`. This method uses the Engine
     directly, no need to pass a Session object.
 
     :type jobs: list
-    :param jobs: a list of :class:`~msnoise.msnoise_table_def.Job` to insert.
+    :param jobs: a list of :class:`~msnoise.msnoise_table_def.declare_tables.Job` to insert.
     """
     engine = get_engine()
     engine.execute(
@@ -755,11 +765,11 @@ def massive_insert_job(jobs):
 def massive_update_job(session, jobs, flag="D"):
     """
     Routine to use a low level function to update much faster a list of
-    :class:`~msnoise.msnoise_table_def.Job`. This method uses the Job.ref
+    :class:`~msnoise.msnoise_table_def.declare_tables.Job`. This method uses the Job.ref
     which is unique.
 
     :type jobs: list
-    :param jobs: a list of :class:`~msnoise.msnoise_table_def.Job` to update.
+    :param jobs: a list of :class:`~msnoise.msnoise_table_def.declare_tables.Job` to update.
     :type flag: str
     :param flag: The destination flag.
     """
@@ -777,7 +787,7 @@ def massive_update_job(session, jobs, flag="D"):
 
 def is_next_job(session, flag='T', jobtype='CC'):
     """
-    Are there any :class:`~msnoise.msnoise_table_def.Job` in the database,
+    Are there any :class:`~msnoise.msnoise_table_def.declare_tables.Job` in the database,
     with flag=`flag` and jobtype=`type`
 
     :type session: :class:`sqlalchemy.orm.session.Session`
@@ -789,7 +799,7 @@ def is_next_job(session, flag='T', jobtype='CC'):
     :param flag: Status of the Job: "T"odo, "I"n Progress, "D"one.
 
     :rtype: bool
-    :returns: True if at least one :class:`~msnoise.msnoise_table_def.Job`
+    :returns: True if at least one :class:`~msnoise.msnoise_table_def.declare_tables.Job`
         matches, False otherwise.
     """
     job = session.query(Job).with_hint(Job, 'USE INDEX (job_index2)').\
@@ -803,7 +813,7 @@ def is_next_job(session, flag='T', jobtype='CC'):
 
 def get_next_job(session, flag='T', jobtype='CC'):
     """
-    Get the next :class:`~msnoise.msnoise_table_def.Job` in the database,
+    Get the next :class:`~msnoise.msnoise_table_def.declare_tables.Job` in the database,
     with flag=`flag` and jobtype=`jobtype`. Jobs of the same `type` are grouped
     per day. This function also sets the flag of all selected Jobs to "I"n
     progress.
@@ -817,7 +827,7 @@ def get_next_job(session, flag='T', jobtype='CC'):
     :param flag: Status of the Job: "T"odo, "I"n Progress, "D"one.
 
     :rtype: list
-    :returns: list of :class:`~msnoise.msnoise_table_def.Job`
+    :returns: list of :class:`~msnoise.msnoise_table_def.declare_tables.Job`
     """
     tmp = []
     while not len(tmp):
@@ -837,7 +847,7 @@ def get_next_job(session, flag='T', jobtype='CC'):
 
 def is_dtt_next_job(session, flag='T', jobtype='DTT', ref=False):
     """
-    Are there any DTT :class:`~msnoise.msnoise_table_def.Job` in the database,
+    Are there any DTT :class:`~msnoise.msnoise_table_def.declare_tables.Job` in the database,
     with flag=`flag` and jobtype=`jobtype`. If `ref` is provided, checks if a
     DTT "REF" job is present.
 
@@ -869,7 +879,7 @@ def is_dtt_next_job(session, flag='T', jobtype='DTT', ref=False):
 
 def get_dtt_next_job(session, flag='T', jobtype='DTT'):
     """
-    Get the next DTT :class:`~msnoise.msnoise_table_def.Job` in the database,
+    Get the next DTT :class:`~msnoise.msnoise_table_def.declare_tables.Job` in the database,
     with flag=`flag` and jobtype=`jobtype`. Jobs are then grouped per station
     pair. This function also sets the flag of all selected Jobs to "I"n
     progress.

msnoise/msnoise_admin.py
@@ -148,7 +148,17 @@ from flask_wtf import Form
 
 from .api import *
 from .default import default
-from .msnoise_table_def import *
+
+from .msnoise_table_def import declare_tables
+
+
+# Declare mapped tables
+sqlschema = declare_tables()
+Filter = sqlschema.Filter
+Job = sqlschema.Job
+Config = sqlschema.Config
+Station = sqlschema.Station
+DataAvailability = sqlschema.DataAvailability
 
 
 class GenericView(BaseView):

commit 1f9eb1184b09ce05d8d571c426d8cc3081774748

Fix database table prefix regression
Commit 6cefc25 introduced a regression for plugins on database tables
creation.  This commit simplifies the changes made by 6cefc25 and allows
to retain compatibility with existing plugins.

With this commit, msnoise_table_def.py by default defines module classes
mapped to relational database tables using the prefix defined in the
db.ini file (or an empty prefix if the file is missing or not readable).
The only case where this behaviour is not desirable is when initialising
a new database, from s000installer.py.  In this case, we simply redefine
the mapped classes using the new suffix to use.

msnoise/api.py
@@ -30,17 +30,7 @@ from obspy import read_inventory
 from obspy.io.xseed import Parser
 from obspy.geodetics import gps2dist_azimuth
 
-from .msnoise_table_def import declare_tables
-
-
-# Declare mapped tables
-sqlschema = declare_tables()
-Filter = sqlschema.Filter
-Job = sqlschema.Job
-Config = sqlschema.Config
-Station = sqlschema.Station
-DataAvailability = sqlschema.DataAvailability
-
+from .msnoise_table_def import Filter, Job, Station, Config, DataAvailability
 
 
 def get_tech():

msnoise/msnoise_admin.py
@@ -149,16 +149,7 @@ from flask_wtf import Form
 from .api import *
 from .default import default
 
-from .msnoise_table_def import declare_tables
-
-
-# Declare mapped tables
-sqlschema = declare_tables()
-Filter = sqlschema.Filter
-Job = sqlschema.Job
-Config = sqlschema.Config
-Station = sqlschema.Station
-DataAvailability = sqlschema.DataAvailability
+from .msnoise_table_def import Filter, Job, Station, Config, DataAvailability
 
 
 class GenericView(BaseView):

msnoise/msnoise_table_def.py
@@ -50,8 +50,8 @@ def declare_tables(prefix=None):
         prefix = read_prefix()
 
     # Define the namedtuple to return
-    sqlschema = namedtuple('SQLSchema', ['Base', 'Filter', 'Job', 'Station',
-        'Config', 'DataAvailability'])
+    sqlschema = namedtuple('SQLSchema', ['Base', 'PrefixerBase', 'Filter',
+        'Job', 'Station', 'Config', 'DataAvailability'])
 
     # Create the SQLAlchemy base and subclass it to prefix the table names
     Base = declarative_base()
@@ -296,5 +296,11 @@ def declare_tables(prefix=None):
 
     ########################################################################
 
-    return sqlschema(Base, Filter, Job, Station, Config, DataAvailability)
+    return sqlschema(Base, PrefixerBase,
+                     Filter, Job, Station, Config, DataAvailability)
     # end of declare_tables()
+
+
+# These module objects only use the prefix defined in db.ini.
+# They should be re-defined if the prefix is to be changed.
+Base, PrefixerBase, Filter, Job, Station, Config, DataAvailability = declare_tables()

msnoise/scripts/msnoise.py
@@ -7,7 +7,7 @@ import time
 import click
 import pkg_resources
 
-from ..msnoise_table_def import declare_tables
+from ..msnoise_table_def import DataAvailability
 from ..api import update_station
 
 
@@ -382,8 +382,6 @@ def populate(fromda):
     """Rapidly scan the archive filenames and find Network/Stations"""
     if fromda:
         logging.info("Overriding workflow...")
-        sqlschema = declare_tables()
-        DataAvailability = sqlschema.DataAvailability
         db = connect()
         stations = db.query(DataAvailability.net, DataAvailability.sta). \
             group_by(DataAvailability.net, DataAvailability.sta)