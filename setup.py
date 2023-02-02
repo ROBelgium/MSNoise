6b1f87992d8585c873cd95f673e0d6d695efffe4
ThomasLecocq <thlecocq@gmail.com>
2023-02-02 12:04:57 +0100

    update tests & include requirements

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(version=get_git_version(),
           'jinja2',
           'scandir',  # useful for python < 3.5
           'logbook',
-          'pytables', # not sure it's still required as I'm using xarray now
           'xarray'  # new in 2.0
       ],
       extras_require={
